d967dbf816de470f9afc7de1f7d7fc3764af59ce
ruchiraw <ruchira.wageesha@gmail.com>
2015-03-24 04:32:18 +0530

    updating

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 var dust = require('dust')();
 var serand = require('serand');
-
-var cdn = serand.configs['autos-images'];
+var utils = require('autos-utils');
 
 var user;
-
-var update = function (data) {
-    var photos = data.photos;
-    if (!photos) {
-        return;
-    }
-    var i;
-    var length = photos.length;
-    for (i = 0; i < length; i++) {
-        photos[i] = cdn + photos[i];
-    }
-    return data;
-};
 
 dust.loadSource(dust.compile(require('./template'), 'auto-details'));
 
@@ -28,7 +14,7 @@ module.exports = function (sandbox, fn, options) {
         },
         dataType: 'json',
         success: function (data) {
-            dust.render('auto-details', update(data), function (err, out) {
+            dust.render('auto-details', utils.cdn(data), function (err, out) {
                 sandbox.append(out);
                 if (!fn) {
                     return;
