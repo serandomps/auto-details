a9c70e730109c6d752a00f802b83d3c7125f939d
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-10-12 19:33:21 +0530

    updating

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var dust = require('dust')();
 var serand = require('serand');
-var autils = require('autos-utils');
 var utils = require('utils');
+var Vehicle = require('vehicle-service');
 
 require('gallery');
 
@@ -10,42 +10,36 @@ var user;
 dust.loadSource(dust.compile(require('./template'), 'auto-details'));
 
 module.exports = function (sandbox, fn, options) {
-    $.ajax({
-        url: utils.resolve('autos://apis/v/vehicles/' + options.id),
-        dataType: 'json',
-        success: function (data) {
-            dust.render('auto-details', autils.cdn800x450(data), function (err, out) {
-                sandbox.append(out);
-                if (!fn) {
-                    return;
-                }
-                fn(false, {
-                    clean: function () {
-                        $('.auto-details', sandbox).remove();
-                    },
-                    done: function () {
-                        var i;
-                        var o = [];
-                        var photos = data.photos;
-                        var length = photos.length;
-                        var photo;
-                        for (i = 0; i < length; i++) {
-                            photo = photos[i];
-                            o.push(photo.url);
-                        }
-                        blueimp.Gallery(o, {
-                            container: $('.blueimp-gallery-carousel', sandbox),
-                            carousel: true
-                        });
-                    }
-                });
-            });
-        },
-        error: function () {
-            fn(true, function () {
-
-            });
+    Vehicle.findOne({id: options.id, images: '800x450'}, function (err, vehicle) {
+        if (err) {
+            return fn(true, serand.none);
         }
+        dust.render('auto-details', vehicle, function (err, out) {
+            sandbox.append(out);
+            if (!fn) {
+                return fn(true, serand.none);
+            }
+            fn(false, {
+                clean: function () {
+                    $('.auto-details', sandbox).remove();
+                },
+                done: function () {
+                    var i;
+                    var o = [];
+                    var photos = vehicle.photos;
+                    var length = photos.length;
+                    var photo;
+                    for (i = 0; i < length; i++) {
+                        photo = photos[i];
+                        o.push(photo.url);
+                    }
+                    blueimp.Gallery(o, {
+                        container: $('.blueimp-gallery-carousel', sandbox),
+                        carousel: true
+                    });
+                }
+            });
+        });
     });
 };
 
